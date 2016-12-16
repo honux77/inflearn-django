17915d6063c9ccfd9bf067cc917a2d0547144bc6
Hoyoung Jung <honux77@gmail.com>
2016-12-16 17:36:44 +0900

    hello example

--- myproj1/lotto/views.py
+++ myproj1/lotto/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
+def index(request):
+    str = "<h1>Hello inflearn</h1><p>%s</p>" % datetime.now().isoformat()
+    return HttpResponse(str)
