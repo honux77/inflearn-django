4deb74b8cdb8e09ba36b32f76591012c8b44b1d4
Hoyoung Jung <honux77@gmail.com>
2016-12-17 16:53:34 +0900

    complete mtv basic

--- myproj1/lotto/views.py
+++ myproj1/lotto/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 # Create your views here.
 def index(request):
     posts = Post.objects.filter(update_date__lte=timezone.now()).order_by('-update_date')
     return render(request, 'lotto/lotto_view.html', {'posts': posts})
+
+def detail(request, pk):    
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'lotto/detail.html', {'post': post})
--- myproj1/myproj1/urls.py
+++ myproj1/myproj1/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
+from lotto import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('lotto.urls')),
+    url(r'^$', views.index, name='lotto_index'),    
+    url(r'^lotto/(?P<pk>[0-9]+)/$', views.detail, name='lotto_detail'),
 ]
