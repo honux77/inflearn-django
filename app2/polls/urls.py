010b4854ca8c452003ecea9328a9d4685a25f775
Hoyoung Jung <honux77@gmail.com>
2016-12-31 00:39:02 +0900

    Complete app 2-3

--- app2/polls/urls.py
+++ app2/polls/urls.py
@@ -1,6 +1,10 @@
+# polls/urls.py
 from django.conf.urls import url
 
 from . import views
 urlpatterns = [
-    url(r'^$', views.index, name = 'index')
+    url(r'^$', views.index, name = 'index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name = "detail"),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name = "results"),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name = "vote"),
 ]
