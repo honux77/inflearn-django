7a837864f098bfee6bca444d6cccd8693e9b287e
Hoyoung Jung <honux77@gmail.com>
2017-01-08 10:16:02 +0900

    Add simple-test

--- object-test/models.py
+++ object-test/models.py
@@ -14,11 +14,10 @@ class Article(Text):
         self.text = text
 
     def __str__(self):
-        return "Article %s %s" % (self.title, self.text)
+        return "Article: %s" % self.title
 
 class User:
     numUsers = 0
-    # numArticle = 0
     def __init__(self, name):
         self.numArticle = 0
         self.name = name
@@ -27,10 +26,13 @@ class User:
 
     def write(self, text):
         self.articles.append(text)
-        self.numArticle += 1
+
+    def getNumArticles(self):
+        return len(self.articles)
 
     def __str__(self):
-        return "User: %s %s" % (self.name, self.articles)
+        str_articles = ", ".join([str(t) for t in self.articles])
+        return "User: %s [%s]" % (self.name, str_articles)
 
 t = Article("hello", "This is some text")
 t2 = Article("world", "This is some text2")
@@ -38,4 +40,4 @@ user = User('honux')
 user.write(t)
 user.write(t2)
 print(t, t.getLength())
-print(user, user.numArticle)
+print(user, user.getNumArticles())
