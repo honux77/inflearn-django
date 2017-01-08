71676b2b7a9e88cf6b6dc7e8754e080005db0f92
Hoyoung Jung <honux77@gmail.com>
2017-01-08 10:13:50 +0900

    Modify object lecture code

--- object-test/models.py
+++ object-test/models.py
@@ -1,40 +1,41 @@
+class Text:
+    def __init__(self, str):
+        self.text = str
+
+    def __str__(self):
+        return "Text: " + self.text
+
+    def getLength(self):
+        return len(self.text)
+
+class Article(Text):
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+
+    def __str__(self):
+        return "Article %s %s" % (self.title, self.text)
+
 class User:
     numUsers = 0
+    # numArticle = 0
     def __init__(self, name):
+        self.numArticle = 0
         self.name = name
         self.articles = []
         User.numUsers += 1
 
     def write(self, text):
         self.articles.append(text)
+        self.numArticle += 1
 
     def __str__(self):
-        articles_str = ", ".join(str(item) for item in self.articles)
-        return "%s %s" % (self.name, articles_str)
+        return "User: %s %s" % (self.name, self.articles)
 
-class Text:
-    def __init__(self, string):
-        self.body = string
-
-    def getLength(self):
-        return len(self.body)
-
-    def __str__(self):
-        return self.body
-
-class Article(Text):
-    def __init__(self, title, body):
-        self.title = title
-        self.body = body
-    def __str__(self):
-        return "[%s] %s" % (self.title, self.body)
-
-# t1 = Text('Hello')
-t1 = Article("Good day", "It is good day to learn!")
-
-user = User('Honux')
-user.write(t1)
-print("Number of User: ",User.numUsers)
-print("User: ", user)
-user2 = User('Crong')
-print("Number of User: ",User.numUsers)
+t = Article("hello", "This is some text")
+t2 = Article("world", "This is some text2")
+user = User('honux')
+user.write(t)
+user.write(t2)
+print(t, t.getLength())
+print(user, user.numArticle)
