4e75f24276564b074a11c1cb899051ad4bbfb545
Hoyoung Jung <honux77@gmail.com>
2017-03-16 17:00:36 +0900

    Add gen secure key script

---
+++ script/genkey.py
@@ -0,0 +1,14 @@
+"""
+Pseudo-random django secret key generator.
+- Does print SECRET key to terminal which can be seen as unsafe.
+"""
+
+import string
+import random
+
+# Get ascii Characters numbers and punctuation (minus quote characters as they could terminate string).
+chars = ''.join([string.ascii_letters, string.digits, string.punctuation]).replace('\'', '').replace('"', '').replace('\\', '')
+
+SECRET_KEY = ''.join([random.SystemRandom().choice(chars) for i in range(50)])
+
+print(SECRET_KEY)
